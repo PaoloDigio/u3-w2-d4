43de04cdcb4b3366db7c66cf498f6852cf207fd7
PaoloDigio <101996038+PaoloDigio@users.noreply.github.com>
2024-03-21 16:55:19 +0100

    first commit

---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,33 @@
+import { Button, ListGroup } from "react-bootstrap";
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + asin, {
+        method: "DELETE",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWUxYTRlZjRjNTllYzAwMTk5MGQ3MjMiLCJpYXQiOjE3MTA0MjM3NjIsImV4cCI6MTcxMTYzMzM2Mn0.EMx3nhjPaBJEYXN84kCp85qNkqpf7F0Pxu78VqJAcTI",
+        },
+      });
+      if (response.ok) {
+        alert("La recensione è stata elimata!");
+      } else {
+        throw new Error("La recensione non è stata eliminata!");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <ListGroup.Item>
+      {comment.comment}
+      <Button variant="danger" className="ms-2" onClick={() => deleteComment(comment._id)}>
+        Elimina
+      </Button>
+    </ListGroup.Item>
+  );
+};
+
+export default SingleComment;
